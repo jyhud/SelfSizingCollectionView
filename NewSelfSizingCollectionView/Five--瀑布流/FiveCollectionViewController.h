7451dcecfdf3a00536131c3c7fa50c12090d6ca0
admin <zhuofeng.jia@condenast.com.cn>
2017-06-07 18:15:32 +0800

    Vertically scrolling UICollectionView with self-sizing cells only displays half the cells

---
+++ NewSelfSizingCollectionView/Five--瀑布流/FiveCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  FiveCollectionViewController.h
+//  NewSelfSizingCollectionView
+//
+//  Created by 贾卓峰 on 2017/6/6.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FiveCollectionViewController : UICollectionViewController
+
+@end
